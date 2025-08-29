5e91b75ba8970e09f1918c14564ece935df2e27d
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-29 17:59:55 -0600

    model relationships and fixed data types

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from dotenv import load_dotenv
-from flask_bcrypt import bcrypt
+from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -29,7 +29,7 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 db.init_app(app)
-bcrypt.init_app(app)
+bcrypt = Bcrypt(app)
 
 migrate = Migrate(app, db)
 api = Api(app)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
-from marshmallow import Schema, fields
 
 from config import db, bcrypt
 
@@ -8,9 +7,13 @@ class User(db.Model):
 	__tablename__ = 'users'
 
 	id = db.Column(db.Integer, primary_key=True)
-	display_name = db.Columnt(db.String, nullable=False)
+	first_name = db.Columnt(db.String, nullable=False)
 	username = db.Column(db.String, unique=True, nullable=False)
 	_password_hash = db.Column(db.String, nullable=False)
+
+	#relationship
+	rounds = db.relationship("Round", back_populates = "user", cascade="all, delete-orphan")
+	challenge = db.relationship("Challenge", back_populates = "user", cascade="all, delete-orphan")
 	
 	@validates('username')
 	def nomalize_username(self, key, value):
@@ -31,34 +34,43 @@ class User(db.Model):
 			self._password_hash, password.encode('utf-8'))
 
 	def __repr__(self):
-		return f'<User: {self.display_name}, {self.username}>'
+		return f'<User: {self.first_name}, {self.username}>'
 	
 	
 class Course(db.Model):
 	__tablename__ = 'courses'
 
-	id = db.Column(db.integer, primary_key=True)
-	external_course_id = db.Column(db.integer)
-	name = db.Column(db.string)
-	city = db.Column(db.string)
-	state = db.Column(db.string)
-	par_total = db.Column(db.integer)
-	total_yards =db.Column(db.integer)
-	holes = db.Column(db.integer)
-	course_rating = db.Column(db.float)
-	slope_rating = db.Column(db.float)
+	id = db.Column(db.Integer, primary_key=True)
+	external_course_id = db.Column(db.Integer)
+	name = db.Column(db.String)
+	city = db.Column(db.String)
+	state = db.Column(db.String)
+	par_total = db.Column(db.Integer)
+	total_yards = db.Column(db.Integer)
+	holes = db.Column(db.Integer)
+	course_rating = db.Column(db.Float)
+	slope_rating = db.Column(db.Float)
+
+	#relationship
+	course_holes = db.relationship("CourseHole", back_populates="course", cascade="all, delete-orphan")
+	rounds = db.relationship("Round", back_populates="course")
+
 
 	def __repr__(self):
 		return f'<Course: {self.name}, {self.city}, {self.state}, {self.par_total}, {self.total_yards}, {self.holes}, {self.course_rating}, {self.slope_rating}>'
 	
 class CourseHole(db.Model):
-	__tablename__ = 'course-holes'
+	__tablename__ = 'course_holes'
 
-	id = db.Column(db.integer, primary_key=True)
-	course_id = db.Column(db.integer, foreign_key=True)
-	course_hole_number = db.Column(db.integer)
-	par = db.Column(db.integer)
-	yardage = db.Column(db.integer)
+	id = db.Column(db.Integer, primary_key=True)
+	course_hole_number = db.Column(db.Integer, nullable=False)
+	par = db.Column(db.Integer, nullable=False)
+	yardage = db.Column(db.Integer, nullable=False)
+	course_id = db.Column(db.Integer, db.ForeignKey("courses.id", ondelete="CASCADE"), nullable=False)
+
+	#relationship
+	courses = db.relationship("Course", back_populates="course_holes")
+	round_holes = db.relationship("RoundHole", back_populates="course_hole")
 
 	def __repr__(self):
 		return f'<Course Hole: {self.course_hole_number}, {self.par}, {self.yardage}>'
@@ -66,26 +78,35 @@ class CourseHole(db.Model):
 class Round(db.Model):
 	__tablename__ = 'rounds'
 
-	id = db.Column(db.integer, primary_key=True)
-	user_id = db.Column(db.integer, foreign_key=True)
-	course_id = db.Column(db.integer, foreign_key=True)
-	date = db.Column(db.date)
-	tee_box = db.Column(db.string)
-	notes = db.Column(db.text)
+	id = db.Column(db.Integer, primary_key=True)
+	date = db.Column(db.Date, nullable=False)
+	tee_box = db.Column(db.String)
+	notes = db.Column(db.Text)
+	user_id = db.Column(db.Integer, db.ForeignKey("users.id", nullable=False))
+	course_id = db.Column(db.Integer, db.ForeignKey("courses.id", nullable=False))
+
+	#relationship
+	user = db.relationship("User", back_populates="rounds")
+	course = db.relationship("Course", back_populates="rounds")
+	round_holes = db.relationship("RoundHole", back_populates="rounds", cascade="all, delete-orphan")
 
 	def __repr__(self):
 		return f'<Round: {self.date}, {self.tee_box}, {self.notes}>'
 	
 class RoundHole(db.Model):
-	__tablename__ = 'round-holes'
+	__tablename__ = 'round_holes'
 
-	id = db.Column(db.integer, primary_key=True)
-	round_id = db.Column(db.integer, foreign_key=True)
-	course_hole_id = db.Column(db.integer, foreign_key=True)
-	hole_number = db.Column(db.integer)
-	start_distance = db.Column(db.integer)
-	surface = db.Column(db.integer)
-	penalty = db.Column(db.integer)
+	id = db.Column(db.Integer, primary_key=True)
+	hole_number = db.Column(db.Integer)
+	start_distance = db.Column(db.Integer)
+	surface = db.Column(db.Integer)
+	penalty = db.Column(db.Integer, default=0)
+	round_id = db.Column(db.Integer, db.ForeignKey("rounds.id", ondelete="CASCADE"), nullable=False)
+	course_hole_id = db.Column(db.Integer, db.ForeignKey("course_hole.id"), nullable=False)
+
+	#relationship
+	round = db.relationship("Round", back_populates="round_holes")
+	course_hole = db.relationship("CourseHole", back_populates="round_holes")
 
 	def __repr__(self):
 		return f'<Round Hole: {self.hole_number}, {self.start_distance}, {self.surface}, {self.penalty}>'
@@ -93,14 +114,17 @@ class RoundHole(db.Model):
 class Challenge(db.Model):
 	__tablename__ = 'challenges'
 
-	id = db.Column(db.integer, primary_key=True)
-	user_id = db.Column(db.integer, foreign_key=True)
-	title = db.Column(db.string)
-	type = db.Column(db.string)
-	target_number = db.Column(db.integer)
-	start_date = db.Column(db.date)
-	end_date = db.Column(db.date)
-	status = db.Column(db.string)
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String)
+	type = db.Column(db.String)
+	target_number = db.Column(db.Integer)
+	start_date = db.Column(db.Date)
+	end_date = db.Column(db.Date)
+	status = db.Column(db.String)
+	user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+	#relationship
+	user = db.relationship("User", back_populates="challenges")
 
 	def __repr__(self):
 		return f'<Challenge: {self.title}, {self.type}, {self.target_number}, {self.start_date}, {self.end_date}, {self.status}'
