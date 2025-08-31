569b97e2bc7160b08d557b591209e10a9038be99
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-31 16:29:03 -0600

    fixed signup,login,whoami endpoints

--- server/app.py
+++ server/app.py
@@ -3,28 +3,27 @@
 from flask import request, jsonify, make_response
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
-from flask_jwt_extended import create_access_token, get_jwt_identity, verify_jwt_in_request
+from flask_jwt_extended import create_access_token, get_jwt_identity, verify_jwt_in_request, jwt_required
 
 from config import app, db, api, jwt
 from models import *
 from schema import *
 
 
-# Protected Routes
-@app.before_request
-def check_if_logged_in():
-    open_access_list = ['signup', 'login']
-    if (request.endpoint) not in open_access_list and (not verify_jwt_in_request()):
-        return {'error': '401 Unauthorized'}, 401
+# # Protected Routes
+# @app.before_request
+# def check_if_logged_in():
+#     open_access_list = ['signup', 'login']
+#     if (request.endpoint) not in open_access_list and (not verify_jwt_in_request()):
+#         return {'error': '401 Unauthorized'}, 401
 
 # Resources
 class Signup(Resource):
     def post(self):
-        request_json = request.get_json()
-
-        username = request_json.get('username')
-        password = request_json.get('password')
-        display_name = request_json.get('display_name')
+        data = request.get_json() or {}
+        username = data.get('username')
+        password = data.get('password')
+        display_name = data.get('display_name')
 
         user = User(
             username=username,
@@ -36,28 +35,31 @@ class Signup(Resource):
             db.session.add(user)
             db.session.commit()
 
-            access_token = create_access_token(identity=str(user.id))
+            access_token = create_access_token(identity=user.id)
             return make_response(
                 jsonify(token=access_token, user=UserSchema().dump(user)),200)
         
         except IntegrityError:
+            db.session.rollback()
             return {'error': ['422 Unprocessable Entity']}, 422
         
 class WhoAmI(Resource):
+    @jwt_required()
     def get(self):
         user_id = get_jwt_identity()
-        user = User.query.filter(User.id == user_id).first()
+        user = User.query.filter_by(id=user_id).first()
         return UserSchema().dump(user), 200
     
 class Login(Resource):
     def post(self):
-        username = request.get_json()['username']
-        password = request.get_json()['password']
+        data = request.get_json() or {}
+        username = data.get('username')
+        password = data.get('password')
 
-        user = User.query.filter(User.username == username).first()
+        user = User.query.filter_by(username=username).first()
 
         if user and user.authenticate(password):
-            access_token = create_access_token(identity=str(user.id))
+            access_token = create_access_token(identity=user.id)
             return make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
         
         return {'error': ['401 Unauthorized']}, 401
