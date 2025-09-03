bb9b54d35086774a6ae848653cb6ff510d49a6de
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-09-03 13:13:14 -0600

    updated home page and added addRound Link to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom"
 
 export default function Dashboard() {
 	return(
-		<h1>Dashboard Page</h1>
+		<>
+			<h1>Dashboard Page</h1>
+			<Link to={`/rounds/new`}>
+				<button>Add Round</button>
+			</Link>	
+		
+		</>
+		
 	)
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import meeseek from "../assets/images/meeseeks/meeseek.PNG"
 
 export default function Home() {
 	return(
 		<>
 			<h1>Paradox Golf</h1>
+			<h2>Track your golf game and embrace the contradiction</h2>
+
+			<img src={meeseek} width="75%" alt="meeseeks on golf course"/>
+
+			<p>Golf is a game of paradoxes. It always seems like the harder you swing, the worse it gets, the shortest putt can feel like the hardest, and one perfect shot can erase 17 terrible holes or vice versa. Paradox Golf is designed for both casual and serious golfers looking for a fun and engaging way to improve their game. Paradox Golf keeps you aware of your numbers, while also keeping your round lighthearted as golf rounds can sometimes feel a bit absurd.</p>
 		</>
 	);
 };
