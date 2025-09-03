d848e14c2aa1133ba14a089baa7bbd69b3b2cf07
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-09-02 19:58:25 -0600

    added hole input fields

--- client/src/components/RoundFeatures/NewRound/HoleFields.jsx
+++ client/src/components/RoundFeatures/NewRound/HoleFields.jsx
@@ -1,7 +1,57 @@
+import { useState } from "react";
 
 export default function HoleFields(){
-	return(
-		<>
-		</>
-	)
+	const [holeData, setHoleData] = useState({
+		hole_number: 1,
+		par: 3,
+		score: 1,
+	})
+
+	function updateHole(e) {
+		const {name, value} = e.target;
+		setHoleData((prev) => ({ ...prev, [name]: ["hole_number", "par", "score"].includes(name) ? Number(value) : value,
+    }));
+	}
+
+	return (
+    <div className="form_section">
+      <h3 className="h3">Hole</h3>
+      <div className="row">
+        <label className="inlineLabel">
+          Hole #
+		  <input className="input"
+            type="number"
+            name="hole_number"
+            min={1}
+            max={18}
+            value={holeData.hole_number}
+            onChange={updateHole}
+          />
+        </label>
+
+        <label className="inlineLabel">
+          Par
+		  <input className="input"
+            type="number"
+            name="par"
+            min={3}
+            max={5}
+            value={holeData.par}
+            onChange={updateHole} 
+          />
+        </label>
+		
+        <label className="inlineLabel">
+          Score
+		  <input className="input"
+            type="number"
+            name="score"
+            min={1}
+            value={holeData.score}
+            onChange={updateHole}
+          />
+        </label>
+      </div>
+    </div>
+  );
 }
--- client/src/components/RoundFeatures/NewRound/RoundFields.jsx
+++ client/src/components/RoundFeatures/NewRound/RoundFields.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 
-export default function RoundFields(){
+export default function RoundFields() {
 	const [roundData, setRoundData] = useState({
 		course_name: "",
 		course_external_id: "",
@@ -11,79 +12,82 @@ export default function RoundFields(){
 	})
 
 	function updateRound(e) {
-		const {name, value} = e.target;
-		setRoundData((prev) => ({ ...prev, [name]: name === "hole" ? Number(value) : value}));
+		const { name, value } = e.target;
+		setRoundData((prev) => ({ ...prev, [name]: name === "hole" ? Number(value) : value }));
 	}
 
 	return (
-    <div className="round_fields">
-      <h2 className="h3">Round</h2>
+		<div className="form_section">
+			<h3 className="h3">Round</h3>
 
-      <label className="label">
-        Course Name
-        <input className="input"
-          name="course_name"
-          value={roundData.course_name}
-          onChange={updateRound}
-          
-          required
-        />
-      </label>
+			<label className="label">
+				Course Name
+				<input className="input"
+					type="text"
+					name="course_name"
+					value={roundData.course_name}
+					onChange={updateRound}
+					required
+				/>
+			</label>
 
-      <label className="label">
-        Date
-        <input className="input"
-          type="date"
-          name="date"
-          value={roundData.date}
-          onChange={updateRound}
-          
-        />
-      </label>
+			<label className="label">
+				Date
+				<input className="input"
+					type="date"
+					name="date"
+					value={roundData.date}
+					onChange={updateRound}
+				/>
+			</label>
 
-	  <label className="label">
-        Tee
-        <select className="input"
-          name="tee"
-          value={roundData.tee}
-          onChange={updateRound}
-        >
-          <option value="male">Mens Tees</option>
-		  <option value="female">Ladies Tees</option>
-        </select>
-      </label>
+			<label className="label">
+				Tee
+				<select className="input"
+					type="text"
+					name="tee"
+					value={roundData.tee}
+					onChange={updateRound}
+				>
+					<option value="male">Mens Tees</option>
+					<option value="female">Ladies Tees</option>
+				</select>
+			</label>
 
-      <label className="label">
-        Tee Name
-        <input className="input"
-          name="tee_name"
-          value={roundData.tee_name}
-          onChange={updateRound}
-          placeholder="1, 2, 3, white, gold, black, etc."
-        />
-      </label>
+			<label className="label">
+				Tee Name
+				<input className="input"
+					type="text"
+					name="tee_name"
+					value={roundData.tee_name}
+					onChange={updateRound}
+					placeholder="1, 2, 3, white, gold, black, etc."
+				/>
+			</label>
 
-      <label className="label">
-        Holes
-        <select className="input"
-          name="holes"
-          value={roundData.holes}
-          onChange={updateRound}
-        >
-          <option value="Front 9">Front 9</option>
-		  <option value="Back 9">Back 9</option>
-          <option value="18">18</option>
-        </select>
-      </label>
+			<label className="label">
+				Holes
+				<select className="input"
+					type="text"
+					name="holes"
+					value={roundData.holes}
+					onChange={updateRound}
+				>
+					<option value="Front 9">Front 9</option>
+					<option value="Back 9">Back 9</option>
+					<option value="18">18</option>
+				</select>
+			</label>
 
-      <label className="label">
-        Notes
-        <textarea className="textarea"
-          name="notes"
-          value={roundData.notes}
-          onChange={updateRound}  
-        />
-      </label>
-    </div>
-  );
+			<label className="label">
+				Notes
+				<textarea className="textarea"
+					type="text"
+					name="notes"
+					value={roundData.notes}
+					onChange={updateRound}
+				/>
+			</label>
+		</div>
+	);
 }
