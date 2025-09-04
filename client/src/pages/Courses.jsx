1e934cf04c4c9d2dea97034a0a8d5e8e4343211d
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-09-03 18:00:18 -0600

    added course search fetch and list course info on course page

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -1,6 +1,65 @@
+import {useState} from "react";
 
 export default function Courses(){
+	const [query, setQuery] = useState("");
+	const [results, setResults] = useState([]);
+
+	//search golf courses (external API)
+	async function handleSearch(e) {
+    e.preventDefault();
+    if (!query.trim()) return;
+
+    try {
+      const token = localStorage.getItem("token");
+      const res = await fetch(
+        `http://127.0.0.1:5555/courses/search?q=${query}`,
+        {
+          headers: {
+            Accept: "application/json",
+            ...(token ? { Authorization: `Bearer ${token}` } : {}),
+          },
+        }
+      );
+      if (!res.ok) throw new Error(`Search failed: ${res.status}`);
+      const data = await res.json();
+
+      const items =
+        (Array.isArray(data?.courses) && data.courses) ||
+        (Array.isArray(data?.results) && data.results) ||
+        [];
+
+      setResults(items);
+    } catch (error) {
+      console.error("Search failed:", error);
+      setResults([]);
+    }
+  }
+
 	return(
-		<h1>Courses Page</h1>
+		<>
+			<h1>Find Golf Courses</h1>
+			<div>
+				<form onSubmit={handleSearch}>
+					<input type="text"
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+					placeholder="Search courses..."
+					/>
+					<button type="submit">Search</button>
+				</form>
+
+				<div>
+					{results.map((course) => (
+						<div key={course.id}>
+							<h2>{course?.course_name}</h2>
+							<p>Course # {course?.id}</p>
+							<p>{course.location?.address}</p>
+						</div>
+						)
+					)}
+				</div>
+			</div>
+
+		</>
 	)
 }
